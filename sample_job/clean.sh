0ef0fd4d4e96246d072bea95d1ce82a95f05c0ff
Edward Ost <eost@talend.com>
2018-04-12 09:59:34 -0400

    updated

--- sample_job/clean.sh
+++ sample_job/clean.sh
@@ -10,13 +10,14 @@ declare job_name="${2:-t0_docker_create_customer}"
 declare app_name="${3:-talend_sample_container_app}"
 declare project_name="${4:-SE_DEMO}"
 
-declare usage="./clean <job_id> <job_name> <app_name> <project_name>"
+declare usage="./clean <job_id> [ <job_name> [ <app_name> [ <project_name> ] ] ]"
 [ -z "${job_id}" ] && echo "ERROR: missing job_id: ${usage}" && exit 1
 
 declare volume_root="/home/eost/talend_distro/sample_job/volumes"
 declare job_instance_root="talend/${project_name}/${app_name}/${job_name}/${job_id}"
 
-sudo rm -rf ${volume_root}/${job_instance_root}
+set -x
 sudo rm -f ${volume_root}/${job_instance_root}/out/*
 sudo rm -f ${volume_root}/${job_instance_root}/temp/*
 sudo rm -f ${volume_root}/${job_instance_root}/log/*
+sudo rm -rf ${volume_root}/${job_instance_root}
