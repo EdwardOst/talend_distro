b625cae610b1dca2863e912ca90f286a44da9e03
Edward Ost <eost@talend.com>
2018-06-30 10:44:55 -0400

    updated

--- job2docker/job2docker_listener/job2docker_listener_run.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/antlr-runtime-3.5.2.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.16.jar;../lib/org.talend.dataquality.parser.jar;../lib/talend_file_enhanced_20070724.jar;../lib/talendcsv.jar;job2docker_listener_0_1.jar;job2docker_0_1.jar;' se_demo.job2docker_listener_0_1.job2docker_listener  --context=Default %* 
--- push_to_docker/push_to_docker/push_to_docker_run.bat
+++
@@ -1,3 +0,0 @@
-%~d0
-cd %~dp0
-java -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/antlr-runtime-3.5.2.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.16.jar;../lib/org.talend.dataquality.parser.jar;../lib/talend_file_enhanced_20070724.jar;push_to_docker_0_1.jar; se_demo.push_to_docker_0_1.push_to_docker  --context=Default %* 
